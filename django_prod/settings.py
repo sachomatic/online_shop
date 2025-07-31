70a96ad35a2ad02483fd9d7104f278496a404d34
Sacha Tardat <sacha.tardat@gmail.com>
2025-07-31 09:59:43 +0200

    Allowed all networks hosts

--- django_prod/settings.py
+++ django_prod/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-%^3838#y*#gtp-8$oz(xhtre4b%!24!f4pqp!cuza74t$6l(23
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
